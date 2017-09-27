02f972e5c91e3a124ef4428ae3fb98e07b30de35
Vitalii Holdysh <vitaliygoldish@gmail.com>
2017-09-28 00:16:59 +0300

    Disable security check for PublicService. Configure Zuul rotes

--- public-service/src/main/java/com/news/PublicController.java
+++ public-service/src/main/java/com/news/PublicController.java
@@ -1,19 +1,23 @@
 package com.news;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
+/**
+ * The controller represents silly endpoint, that returns a "news" payload
+ * on each request.
+ * It was designed with "no security at all" - just to see how to configure
+ * Zuul in this situation.
+ */
 @RestController
 @RequestMapping("/")
 public class PublicController {
 
     @RequestMapping(method = RequestMethod.GET)
-    @PreAuthorize("permitAll")
     public String news() {
-        return "Breaking news! The time is: " + new Date();
+        return "Breaking news! The time is: " + LocalDateTime.now();
     }
 }
